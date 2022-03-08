ac980e14c6b99d391dc16e69f2a8fc01838c111b
unqcode <ugarcia65@ymail.com>
2022-03-08 22:14:31 +0000

    While Looping

---
+++ custif/cust_if_script.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""First Custom Code. Just trying out If, elif, Else."""
+
+issues = input("Describe the type of issue") #user will need to describe type of issue
+
+if issues: "vpn"
+    print ("restart your computer")
+else issues i
+
