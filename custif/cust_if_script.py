9a2366a1a21d7511a1bde1a680c33b4cb1c2eef3
unqcode <ugarcia65@ymail.com>
2022-03-09 01:05:57 +0000

    Learning to troubleshoot

--- custif/cust_if_script.py
+++ custif/cust_if_script.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """First Custom Code. Just trying out If, elif, Else."""
 
-issues = input("Describe the type of issue") #user will need to describe type of issue
+issues = input("Describe the type of issue:") #user will need to describe type of issue
 
-if issues: "vpn"
+if issues  == "vpn":
     print ("restart your computer")
-else issues i
+elif issues == "Locked out":
+    print ("submit a ticket here: https://fanduel.atlassian.net/servicedesk/customer/portal/8/group/30/create/215")
+elif issues == "Other":
+    print ("Submit a ticket here: https://fanduel.atlassian.net/servicedesk/customer/portal/8/group/62/create/219")
 
