c09b6a7e7977b286aa4b3fabc6478fb16fd57e77
unqcode <ugarcia65@ymail.com>
2022-03-09 04:29:11 +0000

    mixed lists

--- mix-list/mixlist01.py
+++ mix-list/mixlist01.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 
-my_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+### How to make list.####
 
-print(my_list[3:5])
+# my_list = [ "192.168.0.5", 5060, "UP" ] #my_list is a word that has value isnide brackets that forms a list.
+
+# print("The first item in the list (IP): " + my_list[0] ) #add + value[number of index] to display info (pull first item)
+
+# print("The second item in the list (port): " + str(my_list[1]) ) #add + str(value[number of index]). str is needed as index is an integer and need to combine as str.
+
+# print("The last item in the list (state): " + my_list[2] ) #same as line:7 (pull last item)
 
 
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+print("IP addresses: " + iplist[3] + ", and " + iplist[4])
 
